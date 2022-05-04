59d13643b01c5d3e4b7b40587f6b5a8a748d6825
mark ho <mark@uuu.com>
2022-05-04 10:28:49 +0800

    add MockBean test

--- src/main/java/com/uuu/demo1/RootController.java
+++ src/main/java/com/uuu/demo1/RootController.java
@@ -1,9 +1,13 @@
 package com.uuu.demo1;
 
 
+import com.uuu.demo1.services.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class RootController {
@@ -11,5 +15,11 @@ public class RootController {
     public String home123(Model model) {
         model.addAttribute("message","Hi, FET with Spring Boot");
         return "home"; // template filename
+    }
+    @Autowired
+    private GreetingService service;
+    @RequestMapping("/greeting")
+    public @ResponseBody String greeting() {
+        return service.greet();
     }
 }
